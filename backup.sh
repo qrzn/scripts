53bc8322593a77192a9b75ca23884dc6d6700f13
qrzn <qrzn@riseup.net>
2022-09-16 17:48:37 +0200

    minor tweaks

--- backup.sh
+++ backup.sh
@@ -2,12 +2,15 @@
 # backup tool for dotfiles
 clear
 echo "running backup script...yay"
-cp -rv ~/.dosbox ~/git/.fls/
-cp -rv ~/.vimrc ~/git/.fls/
-cp -rv ~/.muttrc ~/git/.fls/
-cp -rv ~/.bashrc ~/git/.fls/
-cd ~/git/.fls;
+cd ~/git/.fls
+rm -rfv ~/git/.fls/.dosbox && cp -rv ~/.dosbox ~/git/.fls/
+rm -rfv ~/git/.fls/.vimrc && cp -rv ~/.vimrc ~/git/.fls/
+rm -rfv ~/git/.fls/.muttrc && cp -rv ~/.muttrc ~/git/.fls/
+rm -rfv ~/git/.fls/.bashrc && cp -rv ~/.bashrc ~/git/.fls/
+rm -rfv ~/git/.fls/.startpage && cp -rv ~/.startpage ~/git/.fls
+
+cd ~/git/.fls
 git add . 
-git commit -m 'changed stuff';
-git push;
-echo 'backup complete!';
+git commit -m 'automated backup script message'
+git push
+echo 'backup complete!'
