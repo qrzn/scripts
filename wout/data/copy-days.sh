7ad602c585512ec8c4c8ac830709dbde3c11002e
qrzn <qrzn@riseup.net>
2022-09-27 00:03:08 +0200

    added functions to wout, less code

---
+++ wout/data/copy-days.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+for num in {2..21}; do cp day1.sh day21.sh; done
--- wout/data/workout1/day11.sh
+++ wout/data/workout1/day11.sh
@@ -1,102 +1,137 @@
 #!/bin/bash
 
+# variables
 data='/home/jan/git/scripts/wout/data/workout1/'
-anykey="read -rsp $'Press any key to continue...\n' -n1 key"
+
+# functions
+press (){ 
+	read -rsp $'Press any key to continue...\n' -n1 key
+}
+bell () {
+	printf '\a'
+}
+press
 start=$MINUTES
 clear
 echo 'Welcome to Day 1!'
-read -rsp $'Press any key to continue...\n' -n1 key
-
-# ROUND ONE
-# first excercise
+press
+# WARMUP
 echo 'WARMUP'
 echo '30sec high knees'
 sleep 30
+bell 
 echo '30sec mountain climbers'
 sleep 30
+bell
 echo '30 sec crab walk'
 sleep 30
-read -rsp $'Press any key to continue...\n' -n1 key
+bell
+press
 clear
+# ROUND I
 echo 'ROUND I'
 echo '20x hip raises'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '2.30min shadow boxing'
 sleep 150
+bell
 echo 'done'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15x leg raises'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15x isolated crunches left/right'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '20x knee kicks'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '45x biceps curls'
 echo 'Round 1 complete!'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 clear
+# ROUND II
 echo 'ROUND II'
 echo '15x spiderman pushups'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15x skater jumps'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15x crunches'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15x toe touches'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '2.30mins front rack stretch'
 sleep 150
+bell
 echo 'done'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '45x biceps curls'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo 'round 2 complete!'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 clear
+# ROUND III
 echo 'ROUND III'
 echo '20x jump squats'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15x lunges'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15x knees to elbows'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '10 dive bombers'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15 pushup jumps'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '45 biceps curls'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo 'ROUND III complete!'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 clear
+# ROUND IV
 echo 'ROUND IV'
 echo '15x diamond pushups'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15x pushups'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15x hollow rocks'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15 spartan pushups'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '45 biceps curls'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo 'ROUND IV complete!'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 clear
+# ROUND V
 echo 'ROUND V'
 echo '15x crab walk'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '30x high knees'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '20x lunges'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '15x leg raises'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
 echo '30x biceps curls'
-read -rsp $'Press any key to continue...\n' -n1 key
+press
+clear
+echo 'STRETCHING'
+press
+echo ' 30 sec straddle stretch'
+sleep 30
+bell
+echo '30 sec front rack stretch'
+sleep 30
+bell
+echo ' 30 sec straddle stretch'
+sleep 30
+bell
+echo '30 sec front rack stretch'
+sleep 30
+bell
+echo "Well done! You completed the workout for today. See you tomorrow!"
+press
 clear
 
 #timer
 duration=$(( MINUTES - start ))
 echo "SUMMARY"
 echo "Your workout took $MINUTES minutes"
+echo "You burned x calories."
