a6c8b2238ea8e4938ea45b46971ba9e921db9411
qrzn <qrzn@riseup.net>
2022-10-21 11:16:27 +0200

    added linking script

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # scripts
 
-a collection of my scripts, so far it includes
+dirty scripts for dirty work, use at your own:w risk and peril!
+
+so far
 
 * ext - an archive extractor
 * games - game wizard, a game launcher
+* link - links dotfiles from repo to $HOME
 * backup - a nice tool to keep your dotfiles up to date
+* timer - a basic timer
 * wout - workout program
+
+and some minor things not really worth noting.
---
+++ link.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# script to automatically link dotfiles to thy home folder
+# deleting existing dotfiles...
+echo "deleting existing dotfiles..." 
+rm -v $HOME/.bashrc
+rm -v $HOME/.dosbox/
+rm -v $HOME/.muttrc
+rm -v $HOME/.startpage
+rm -v $HOME/.vimrc
+rm -v $HOME/.zshrc
+echo "done, creating links now..."
+# creating links
+ln -s $HOME/git/.fls/.bashrc $HOME/.bashrc
+ln -s $HOME/git/.fls/.dosbox/ $HOME/.dosbox
+ln -s $HOME/git/.fls/.muttrc $HOME/.muttrc
+ln -s $HOME/git/.fls/.startpage/ $HOME/.startpage/
+ln -s $HOME/git/.fls/.vimrc $HOME/.vimrc
+ln -s $HOME/git/.fls/.zshrc $HOME/.zshrc
+echo "done!"
+
