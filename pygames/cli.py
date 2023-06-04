7b17f45618326ec6a4ca6d7df54e6b0f03936cdb
qrzn <qrzn@riseup.net>
2023-06-04 17:57:44 +0200

    new stuff

--- pygames/cli.py
+++ pygames/cli.py
@@ -2,11 +2,129 @@ import os
 
 # Dictionary to hold game categories and their associated games
 game_categories = {
-    "DOS": ["Albion", "Arena", "Ascendancy", "Centurion", "Civilization","Caster of Magic", "Commander Keen", "Daggerfall", "Darklands", "Duke Nukem 3D", "Dune II", "Master of Magic", "Master of Orion", "Master of Orion II - Battle at Antares", "Pirates!", "Quake", "Siedler 2", "Shadow Warrior" "Ultima", "Veil of Darkness", "Zeliard"],
-    "Wine": ["Arcanum", "Emperor", "Diablo II", "Fallout", "Fallout 2", "Fallout New Vegas" "Stronghold", "Pharaoh", "Weltwunder", "Zeus"],
-    "Steam": ["Game 2"],
-    "Native": ["Exult", "Factorio", "Daggerfall Unity", "Return to the Roots", "Widelands"],
-    "Emulators": ["Game 2"],
+    "DOS": [
+        "Albion",
+        "Arena",
+        "Ascendancy",
+        "Centurion",
+        "Civilization",
+        "Caster of Magic",
+        "Commander Keen", 
+        "Daggerfall", 
+        "Darklands", 
+        "Duke Nukem 3D", 
+        "Dune II", 
+        "Master of Magic", 
+        "Master of Orion", 
+        "Master of Orion II - Battle at Antares", 
+        "Pirates!", 
+        "Ultima", 
+        "Quake", 
+        "Siedler 2", 
+        "Shadow Warrior", 
+        "Ultima", 
+        "Ultima Underworld",
+        "Veil of Darkness", 
+        "Zeliard"
+        ],
+    "RPG": [
+        "Albion",
+        "Arena",
+        "Arcanum",
+        "Daggerfall",
+        "Daggerfall Unity",
+        "Darklands",
+        "Diablo II",
+        "Exult",
+        "Fallout",
+        "Fallout 2",
+        "Fallout New Vegas",
+        "OpenMW",
+        "Darklands",
+        "Ultima 4-6",
+        "Ultima Underworld",
+        "Veil of Darkness",
+        ],
+    "Strategy": [
+        "Ascendancy",
+        "Civilization",
+        "Centurion",
+        "Dungeon Keeper II",
+        "Factorio",
+        "Emperor",
+        "Caster of Magic",
+        "Master of Magic",
+        "Master of Orion",
+        "Master of Orion II - Battle at Antares",
+        "Siedler 2",
+        "Stronghold",
+        "Stronghold Crusader",
+        "Pharaoh",
+        "War Wind",
+        "Weltwunder",
+        "Zeus",
+        ],
+    "Action": [
+        "Albion",
+        "Arena",
+        "Commander Keen",
+        "Duke Nukem 3D",
+        "Half-Life",
+        "Pirates!",
+        "Quake",
+        ],
+    "4X": [
+        "Civilization",
+        "Master of Magic",
+        "Master of Orion",
+        "Caster of Magic",
+        "Master of Orion II - Battle at Antares",
+
+        ],
+    "Steam": [
+        "Civilization",
+        "Master of Magic",
+        "Master of Orion",
+        "Caster of Magic",
+        "Master of Orion II - Battle at Antares",
+
+        ],
+    "Indie": [
+        "Don't Starve",
+        "Factorio",
+        "Starbound",
+        "Stardew Valley",
+        ],
+    "Wine": [
+        "Arcanum", 
+        "Emperor", 
+        "Diablo II", 
+        "Dungeon Keeper II", 
+        "Fallout", 
+        "Fallout 2", 
+        "Fallout New Vegas", 
+        "Galactic Civilizations II", 
+        "Stronghold", 
+        "Stronghold Crusader", 
+        "Pharaoh", 
+        "War Wind", 
+        "Weltwunder", 
+        "Zeus"
+        ],
+    "Native": [
+        "Exult", 
+        "Factorio", 
+        "Don't Starve", 
+        "Daggerfall Unity", 
+        "Return to the Roots", 
+        "OpenMW", 
+        "Stardew Valley", 
+        "Starbound", 
+        "The Ur-Quan Masters",
+        "Widelands",
+        "Valhalla"
+        ],
+    "Emulators": ["RetroArch", "ScummVM"],
 }
 
 # Dictionary to hold game commands
@@ -21,28 +139,48 @@ game_commands = {
     "Siedler 2": "dosbox -conf ~/.dosbox/s2.conf",
     "Master of Orion": "dosbox -conf ~/.dosbox/moo.conf",
     "Master of Magic": "dosbox -conf ~/.dosbox/mom.conf",
-    "Ultima": "dosbox -conf ~/.dosbox/ultima.conf",
-    "Daggerfall": "dosbox -conf ~/.dosbox/fall.conf",
-    "Daggerfall": "dosbox -conf ~/.dosbox/fall.conf",
-    "Daggerfall": "dosbox -conf ~/.dosbox/fall.conf",
+    "Ultima 4-6": "dosbox -conf ~/.dosbox/ultima.conf",
+    "Ultima Underworld": "dosbox -conf ~/.dosbox/uuw.conf",
+    "Pirates!": "dosbox -conf ~/.dosbox/pirates.conf",
+    "Quake": "dosbox -conf ~/.dosbox/quake.conf",
+    "Veil of Darkness": "dosbox -conf ~/.dosbox/veil.conf",
     "Daggerfall": "dosbox -conf ~/.dosbox/fall.conf",
     "Daggerfall": "dosbox -conf ~/.dosbox/fall.conf",
     # wine
     "Arcanum": "wine ~/Spiele/wine/Arcanum/Arcanum.exe",
     "Emperor": "wine ~/Spiele/wine/emperor/Emperor.exe",
-    "Diablo II": "wine ~/Spiele/wine/emperor/Emperor.exe",
+    "Diablo II": "wine ~/Spiele/wine/diablo2/Game.exe",
+    "Fallout": "wine ~/Spiele/wine/fallout/FALLOUTW.exe",
+    "Fallout 2": "wine ~/Spiele/wine/fallout2/fallout2.exe",
+    "Dungeon Keeper II": "wine ~/Spiele/wine/dk2/DKII-DX.exe",
+    "Galactic Civilizations II": "wine ~/Spiele/wine/galciv2/Game.exe",
     "Stronghold": "wine ~/Spiele/wine/stronghold/Stronghold.exe",
+    "Stronghold Crusader": "wine ~/Spiele/wine/stronghold/Stronghold.exe",
     "Pharaoh": "wine ~/Spiele/wine/pharaoh/Pharaoh.exe",
+    "War Wind": "wine ~/Spiele/wine/warwind/WW.exe",
     "Weltwunder": "wine ~/Spiele/wine/c4/Game.exe",
+    "Zeus": "wine ~/Spiele/wine/zeus/Zeus.exe",
     # Native
     "Daggerfall Unity": "~/Spiele/dfu/DaggerfallUnity.x86_64",
+    "Don't Starve": "~/Spiele/dontstarve/start.sh",
     "Exult": "exult",
-    "Factorio": "~/Spiele/dfu/DaggerfallUnity.x86_64",
-    "Daggerfall Unity": "~/Spiele/dfu/DaggerfallUnity.x86_64",
+    "Factorio": "~/Spiele/factorio/bin/x64/factorio",
+    "Starbound": "~/Spiele/starbound/start.sh",
+    "Stardew Valley": "~/Spiele/stardew/start.sh",
+    "OpenMW": "openmw-launcher",
     "Widelands": "widelands",
+    "The Ur-Quan Masters": "uqm",
+    "Valhalla": "~/Spiele/valhalla/start.sh",
     # Emulators
     "RetroArch": "retroarch",
+    "ScummVM": "scummvm",
 }
+
+def header():
+    print("-" * 50)
+    print("{:^50}".format("Game Serpent Wizard 1.0"))
+    print("{:^50}".format("by qrzn"))
+    print("-" * 50)
 
 def clear_screen():
     # Function to clear the console screen
@@ -51,8 +189,8 @@ def clear_screen():
 def display_categories():
     # Function to display available game categories
     clear_screen()
-    print("Game Serpent Wizard 1.0")
-    print("Available Categories:")
+    header()
+    print("Choose Thy Categorie:")
     index = 1
     for category in sorted(game_categories.keys()):
         print("{}. {}".format(index, category))
@@ -62,6 +200,7 @@ def display_categories():
 def display_games(category):
     # Function to display games in a specific category
     clear_screen()
+    header()
     print("Games in the '{}' Category:".format(category))
     games = sorted(game_categories[category])
     index = 1
@@ -86,8 +225,8 @@ def launch_game(game):
 # Main game launcher loop
 while True:
     display_categories()
-    category_choice = input("Enter the category number (or 'quit' to exit): ")
-    if category_choice == "quit":
+    category_choice = input("Choose Thy Categorie [1-9] (or 'q' to exit): ")
+    if category_choice == "q":
         break
     elif category_choice.isdigit():
         category_choice = int(category_choice)
@@ -95,9 +234,11 @@ while True:
             categories = sorted(game_categories.keys())
             selected_category = categories[category_choice - 1]
             display_games(selected_category)
-            game_choice = input("Enter the game number (or 'back' to go back): ")
-            if game_choice == "back":
+            game_choice = input("Choose Thy Game number (or go 'b'ack or 'q'uit): ")
+            if game_choice == "b":
                 continue
+            elif game_choice == "q":
+                break
             elif game_choice.isdigit():
                 game_choice = int(game_choice)
                 games = sorted(game_categories[selected_category])
